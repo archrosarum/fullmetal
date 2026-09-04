85fea20a3204886c677c40a55b3430983667202e
Andrew Salois <andrewsalois09@gmail.com>
2026-09-04 05:28:22 -0400

    Delete hyprland-configs directory

--- hyprland-configs/favorite_programs.lua
+++
@@ -1,8 +0,0 @@
--- DEFAULT PROGRAMS --
-    
-    -- Favorite web browser --
-    favorite_web_browser = "zen-browser"
-    -- Favorite file explorer --
-    favorite_file_explorer = "dolphin"
-    -- Favorite terminal --
-    favorite_terminal = "kitty"
